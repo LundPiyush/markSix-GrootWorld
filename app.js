77f6a1b04b66717a0cbd4fbe675f9bb7fff29297
Piyush Lund <piyushlund349@gmail.com>
2022-07-20 21:51:41 +0530

    added styles and script

---
+++ app.js
@@ -0,0 +1,26 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var input = document.querySelector("#input")
+var output = document.querySelector("#ouput")
+var serverUrl = "https://api.funtranslations.com/translate/groot.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getUrl(inputText) {
+    return serverUrl + "?text=" + inputText
+}
+function errorHandler(error){
+    alert("Groot is sleeping, Please come back after some time")
+    console.log(error)
+}
+function clickListener() {
+    //console.log("clicked")
+    var inputText = input.value; // fetches the value for id="input"
+    //console.log(inputText)
+
+    fetch(getUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            ouput.innerText = json.contents.translated})
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickListener)
